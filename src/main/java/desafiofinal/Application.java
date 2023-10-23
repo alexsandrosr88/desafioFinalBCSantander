cd43cf4d323c205f901f99cc3965612f4da3d224
alexsandrosr88 <alexsandrosr@msn.com>
2023-10-22 21:15:15 -0300

    BUGfix CORS

--- src/main/java/desafiofinal/Application.java
+++ src/main/java/desafiofinal/Application.java
@@ -1,9 +1,12 @@
 package desafiofinal;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 @EnableFeignClients
 @SpringBootApplication
 public class Application {
