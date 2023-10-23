99288bd59f25040b1ebb1ae0373dba3921ff401d
alexsandrosr88 <alexsandrosr@msn.com>
2023-10-22 21:01:42 -0300

    Primeira tentativa implementação na nuvem Railway

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO endereco(cep,logradouro,numero,complemento,bairro,cidade,estado)VALUES ('01001-000','Praça da Sé','s/n','lado ímpar','Sé','São Paulo','SP');
-INSERT INTO contato(endereco_id,nome, cpf,telefone,email)VALUES (1,'Fulano de alguma coisa','11111111111','(11) 99999-0000','teste@teste.com');
